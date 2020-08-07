26d82c2905087bc1685248864cd1538103843c86
AdalarasanJ <adalarasan@gmail.com>
2020-08-07 01:52:18 +0100

    Initial Commit

---
+++ src/test/java/org/imsa/APITest.java
@@ -0,0 +1,20 @@
+package org.imsa;
+
+import org.testng.annotations.Test;
+
+public class APITest {
+
+
+    @Test
+    public void postJira(){
+
+        System.out.println("postJira");
+
+    }
+
+    @Test
+    public void deleteTwitter(){
+        System.out.println("deleteTwitter");
+
+    }
+}
---
+++ src/test/java/org/imsa/AppiumTest.java
@@ -0,0 +1,21 @@
+package org.imsa;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+
+
+    @Test
+    public void nativeAppAndroid(){
+
+        System.out.println("nativeAppAndroid");
+
+    }
+
+    @Test
+    public void nativeAppIos(){
+        System.out.println("nativeAppIos");
+
+
+    }
+}
